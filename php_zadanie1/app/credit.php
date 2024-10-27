fcf29b3ec42379d574ab81da5f50d04c71e0c72e
Kamil Polit <47361533+Kamileux@users.noreply.github.com>
2024-10-27 22:25:33 +0100

    Add files via upload

---
+++ php_zadanie1/app/credit.php
@@ -0,0 +1,62 @@
+<?php
+require_once dirname(__FILE__).'/../config.php';
+
+$kwota = $_REQUEST ['kwota'];
+$procent = $_REQUEST ['procent'];
+$lata = $_REQUEST ['lata'];
+
+
+if ( ! (isset($lata) && isset($procent) && isset($kwota))) {
+    
+    $messages [] = 'Błędne wywołanie aplikacji. Brak jednego z parametrów.';
+}
+
+
+if ( $kwota == "") {
+    $messages [] = 'Nie podano kwoty';
+}
+if ( $procent == "") {
+    $messages [] = 'Nie podano oprocentowania';
+}
+if ( $lata == "") {
+    $messages [] = 'Nie podano liczby miesięcy';
+}
+
+
+if (empty( $messages )) {
+    
+    
+    if (! is_numeric( $kwota )) {
+        $messages [] = 'Pierwsza wartość nie jest poprawną kwotą';
+    }
+    
+    if (! is_numeric( $procent)) {
+        $messages [] = 'Druga wartość nie jest poprawna';
+    }
+    
+    if (! is_numeric( $lata)) {
+        $messages [] = 'Trzecia wartość nie jest poprawna';
+    }
+    
+}
+
+include 'credit_view.php';
+
+if (empty ( $messages )) {
+    
+    
+    $kwota = floatval($kwota);
+    $procent = floatval($procent);
+    $lata = intval($lata);
+    $miesiace = $lata * 12 ; 
+ 
+    $miesieczna_rata = ($kwota + $kwota * $procent/100)/$miesiace ; 
+  
+    $messages[] =  "Miesięczna rata wynosi: " . $miesieczna_rata ;
+    
+    
+}
+foreach($messages as $tabelka){
+    echo  $tabelka . "<br/>" ; 
+}
+
---
+++ php_zadanie1/app/credit_view.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" lang="pl">
+<head>
+<meta charset="utf-8" />
+<title>Kalkulator Budzetowy</title>
+</head>
+<body>
+
+<form action="<?php print(_APP_URL);?>/app/credit.php" method="post">
+	<label for="id_x">Kwota</label>
+	<input id="id_x" type="text" name="kwota"/><br />
+	<label for="id_y">Oprocentowanie</label>
+	<input id="id_y" type="text" name="procent"/><br />
+	<label for="id_z">Liczba miesięcy </label>
+	<input id="id_z" type="text" name="lata"/><br />
+	<input type="submit" value="Oblicz" />
+</form>	
+
+
+
+</body>
+</html>
